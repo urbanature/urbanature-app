e79f27b45db032d7638f0cae815d09867a4a5b1c
TotoShampoin <thomasgarnier2010@hotmail.fr>
2022-06-10 09:47:44 +0200

    added search engine

--- src/data_manager/bd.js
+++ src/data_manager/bd.js
@@ -1,3 +1,4 @@
+export const categories = [];
 export const table = [];
 export const flags = {
     loaded: false,
@@ -7,20 +8,26 @@ export const on = {
 };
 
 export const initData = async () => {
-    /** @type {{name,path,marker,table,data}[]} */
+    /** @type {{name,path,marker,table,data,key_path,name_path}[]} */
     const loaded_table = await fetch("/database/json/table.json")
                             .then(res => res.json());
+    const loaded_categories = await fetch("/database/json/categories.json")
+                                .then(res => res.json());
     for(const t of loaded_table) {
         t.table = await fetch(`/database/json/${t.path}/table.json`)
                             .then(res => res.json());
     }
     loaded_table.forEach(t => table.push(t));
+    loaded_categories.forEach(c => categories.push(c));
     flags.loaded = true;
     on.load();
 }
 
 export const fetchData = async (dbName, keyName) => {
     const db = table.find(t => t.path === dbName);
+    if(!db) {
+        throw new Error(`Database ${dbName} not found`);
+    }
     const tab = db.table.find(t => t.key === keyName);
     const data = await fetch(`/database/json/${db.path}/data/${tab.path}`)
                             .then(res => res.json());
@@ -28,11 +35,34 @@ export const fetchData = async (dbName, keyName) => {
 }
 
 export const fetchAllData = async (dbName) => {
-    const db = table.find(t => t.name === dbName);
+    const db = table.find(t => t.path === dbName);
     const allData = [];
     for(const t of db.table) {
-        const data = await fetchData(dbName, t.key);
+        const data = await fetchData(db.path, t.key);
         allData.push({key: t.key, name: t.name, data});
     }
     return allData;
 }
+
+export const getDatabases = () => {
+    return table.map(t => t.path);
+}
+export const getTableFromDatabase = (dbName) => {
+    const db = table.find(t => t.path === dbName);
+    if(!db) {
+        throw new Error(`Database ${dbName} not found`);
+    }
+    return db.table;
+}
+export const getTableMetaData = (dbName) => {
+    const db = table.find(t => t.path === dbName);
+    if(!db) {
+        throw new Error(`Database ${dbName} not found`);
+    }
+    return db;
+}
+
+window.fetchData = fetchData;
+window.fetchAllData = fetchAllData;
+window.getTableFromDatabase = getTableFromDatabase;
+window.getTableMetaData = getTableMetaData;
---
+++ src/search.js
@@ -0,0 +1,81 @@
+import * as BASEDATA from "./data_manager/bd.js";
+import { levenstein } from "../vendors_cpy/levenstein.js";
+
+const includes = (a, b) => {
+    return a?.toLowerCase().includes(b?.toLowerCase());
+}
+const equals = (a, b) => {
+    return a?.toLowerCase() === b?.toLowerCase();
+}
+
+export const searchByKeyInDB = async (dbName, keyName, searchText) => {
+    console.log(`searchByKeyInDB(${dbName}, ${keyName}, ${searchText})`);
+    const {table: dbs, key_path, name_path} = BASEDATA.getTableMetaData(dbName);
+    const searchResult = [];
+    let alreadyScanned = undefined;
+    for(let key of [key_path, name_path]) {
+        if(key === keyName) {
+            let tkey;
+            switch(key) {
+                case key_path:
+                    tkey = "key";
+                    break;
+                case name_path:
+                    tkey = "name";
+                    break;
+                default: throw new Error(`unknown key ${key}`);
+            }
+            const some_dbs = dbs.filter(t => includes(String(t[tkey]), searchText));
+            if(some_dbs.length > 0) {
+                for(let db of some_dbs) {
+                    searchResult.push(db);
+                    console.log(`Scanning ${db.key} by shortcut`);
+                    const d = await BASEDATA.fetchData(dbName, db.key);
+                    if(equals(db[tkey], searchText)) {
+                        return d;
+                    } else {
+                        searchResult.push(...d);
+                        alreadyScanned = db;
+                    }
+                }
+            }
+            break;
+        }
+    }
+    for(const db of dbs) {
+        if(db === alreadyScanned) continue;
+        console.log(`Scanning ${db.key}`);
+        const d = await BASEDATA.fetchData(dbName, db.key);
+        const data = d.filter(d => includes(eval(`d.${keyName}`), searchText));
+        if(data) {
+            searchResult.push(...data);
+        }
+    }
+    searchResult.sort((a, b) => {
+        const l = levenstein(eval(`a.${keyName}`), searchText) - levenstein(eval(`b.${keyName}`), searchText);
+        if(l === 0) {
+            return a.key.localeCompare(b.key);
+        }
+        return l;
+    });
+    return searchResult;
+}
+
+export const searchByKeyInDBWithLimit = async (dbName, keyName, searchText, limit) => {
+    const search = await searchByKeyInDB(dbName, keyName, searchText);
+    return search.slice(0, limit);
+}
+
+export const searchByKeyEverywhere = async (keyName, searchText) => {
+    const searchResult = [];
+    for(const dbName of BASEDATA.getDatabases()) {
+        const search = await searchByKeyInDB(dbName, keyName, searchText);
+        searchResult.push({dbName, result: search});
+    }
+    return searchResult;
+}
+
+
+window.searchByKeyInDB = searchByKeyInDB;
+window.searchByKeyInDBWithLimit = searchByKeyInDBWithLimit;
+window.searchByKeyEverywhere = searchByKeyEverywhere;
