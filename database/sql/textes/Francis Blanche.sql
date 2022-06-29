e1e336f041b29c48cff95cca798a407cf3d27962
TotoShampoin <thomasgarnier2010@hotmail.fr>
2022-06-29 14:52:52 +0200

    Updated database

--- database/sql/textes/Francis Blanche.sql
+++ database/sql/textes/Francis Blanche.sql
@@ -1,14 +1,1 @@
-INSERT INTO `textes` (`id`, `name`, `info.author`, `info.source`, `info.category`, `info.contenu`, `geo.type`, `geo.coordinates`, `geoposition`, `meta.img`, `meta.address`) VALUES ('37', 'Le carnaval des animaux ', 'Francis Blanche ', 'http://musique.ac-dijon.fr/auditor/2004/stsaens/carnaval.pdf', '', '"Au jardin des plantes,
-Ainsi nommé d''ailleurs
-A cause des animaux
-Qu''on y a rassemblés,
-Au jardin des plantes,
-Une étrange ardeur semble régner.
-On décore. On festonne.
-On visse. On cloue. On plante."
-Le castor construit des tréteaux.
-La grue porte des fardeaux.
-Le python accroche des tableaux.
-Car ce soir, au Jardin des plantes,
-C''est la Grand-fête éblouissante :
-Le Carnaval des Animaux.', 'Point', '[2.363922, 48.844465]', POINT(2.363922, 48.844465), '['''', '''']', '');
+INSERT INTO `textes` (`id`, `name`, `info.author`, `info.source`, `info.category`, `info.contenu`, `meta.img`, `meta.adresse`, `geo`) VALUES ('37', 'Le carnaval des animaux', 'Francis Blanche', 'http://musique.ac-dijon.fr/auditor/2004/stsaens/carnaval.pdf', '', '"Au jardin des plantes,Ainsi nommé d''ailleursA cause des animauxQu''on y a rassemblés,Au jardin des plantes,Une étrange ardeur semble régner.On décore. On festonne.On visse. On cloue. On plante."Le castor construit des tréteaux.La grue porte des fardeaux.Le python accroche des tableaux.Car ce soir, au Jardin des plantes,C''est la Grand-fête éblouissante :Le Carnaval des Animaux.', '', 'Jardin des Plantes, 57 Rue Cuvier, 75005 Paris', '{''type'': ''Point'', ''coordinates'': [2.363922, 48.844465]}');
